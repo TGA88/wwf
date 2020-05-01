aa1c6e7a7f4f23e8364726c093f05f0c9303cd8c
TGA88 <supawut.thomas@gmail.com>
2020-05-01 18:07:22 +0700

    init IdentityAPI

---
+++ Services/Identity/Identity.API/Config.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+using IdentityServer4;
+
+namespace wwf.Services.Identity.API
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> Ids =>
+            new IdentityResource[]
+            {
+                 new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+
+        public static IEnumerable<ApiResource> Apis =>
+            new ApiResource[]
+            {   new ApiResource("organizer","Organizer Service"),
+                new ApiResource ("catalog","Catalog Service"),
+                 new ApiResource("bi","BI Service"),
+                new ApiResource("userinfo","User Information Service"),
+                new ApiResource("recording","Recording Service"),
+                new ApiResource("reward","Reward Service"),
+                new ApiResource("coin","Coin Service")
+                };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {  new Client{
+                ClientId="publisher-js.wwf.com",
+                ClientName="PubliherWebClient",
+                AllowedGrantTypes= GrantTypes.Code,
+                RequireClientSecret=false,
+                RequirePkce=true,
+                RedirectUris = { "http://localhost:5003/callback.html" },
+                PostLogoutRedirectUris = { "http://localhost:5003/index.html" },
+                AllowedCorsOrigins =     { "http://localhost:5003" },
+                AllowedScopes = {
+                                    IdentityServerConstants.StandardScopes.OpenId,
+                                    IdentityServerConstants.StandardScopes.Profile,
+                                    "organizer"
+                                    }
+                }
+                 };
+
+    }
+}
---
+++ Services/Identity/Identity.API/Startup.cs
@@ -0,0 +1,86 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+using wwf.Services.Identity.API.Models;
+using wwf.Services.Identity.API.Services;
+using wwf.Services.Identity.API.Data;
+
+namespace wwf.Services.Identity.API
+{
+    public class Startup
+    {
+        public IWebHostEnvironment Environment { get; }
+
+        public Startup(IWebHostEnvironment environment)
+        {
+            Environment = environment;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // uncomment, if you want to add an MVC-based UI
+            services.AddControllersWithViews();
+
+            services.AddTransient<ILoginService<ApplicationUser>, EFLoginService>();
+
+            //Config Connection for IdentityDbContext(Model of AspNetCoreIdentity) to EF.
+            services.AddDbContext<ApplicationDbContext>(option => { option.UseInMemoryDatabase("IdentityDB"); });
+
+            //add AspnetCoreIdentity to IOC
+            services.AddIdentity<ApplicationUser, IdentityRole>(config => { config.SignIn.RequireConfirmedEmail = true; })
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+
+            var builder = services.AddIdentityServer()
+                .AddInMemoryIdentityResources(Config.Ids)
+                .AddInMemoryApiResources(Config.Apis)
+                .AddInMemoryClients(Config.Clients)
+                .AddAspNetIdentity<ApplicationUser>();
+
+       
+            // not recommended for production - you need to store your key material somewhere secure
+            if (Environment.IsDevelopment())
+            {
+                builder.AddDeveloperSigningCredential();
+            }
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            if (Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // app.UseHsts();
+            }
+
+            // uncomment if you want to add MVC
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseIdentityServer();
+
+            // uncomment, if you want to add MVC
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
